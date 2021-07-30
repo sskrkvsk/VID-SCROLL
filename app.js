3fdeb4d3e2732a99f6611354dba5e92ed14f546b
sskrkvsk <62514297+sskrkvsk@users.noreply.github.com>
2021-07-30 06:52:50 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,10 +4,88 @@
 //offsetTop - A Number, representing the top position of the element, in pixels
 
 // ********** set date ************
-
+const currentYear = document.getElementById("date");
+currentYear.innerHTML = new Date().getFullYear();
+//
+//
+// WORKS
+//
+//
 // ********** close links ************
+const navToggle = document.querySelector(".nav-toggle");
+const linksContainer = document.querySelector(".links-container");
+const links = document.querySelector(".links");
 
+navToggle.addEventListener("click", function () {
+  // getting two heights. container should be 0, links should have some value
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  // if we didn't click yet button and then click - it will show up  our links. Else - we close it.
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`;
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
+//
+//
+//
+// WORKS
+//
+//
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+ 
+window.addEventListener("scroll", function () {
+  const navHeight = navbar.getBoundingClientRect().height;
+  if (window.pageYOffset > navHeight) {
+    navbar.classList.add("fixed-nav");
+  } else {
+    navbar.classList.remove("fixed-nav");
+  }
 
+  if (window.pageYOffset > 500) {
+    topLink.classList.add("show-link");
+  } else {
+    topLink.classList.remove("show-link");
+  }
+});
+//
+//
+//
+// WORKS
+//
+//
 // ********** smooth scroll ************
 // select links
+const scrollLinks = document.querySelectorAll(".scroll-link");
+
+scrollLinks.forEach((link) => {
+  link.addEventListener("click", function (e) {
+    // prevent default
+    e.preventDefault();
+    // navigate to specific spot
+    const id = e.currentTarget.getAttribute("href").slice(1);
+    const element = document.getElementById(id);
+    // calculate the heights
+    const navHeight = navbar.getBoundingClientRect().height;
+    const containerHeight = linksContainer.getBoundingClientRect().height;
+    const fixedNav = navbar.classList.contains("fixed-nav");
+    let position = element.offsetTop - navHeight;
+    // (!fixedNav) = (fixedNav = false)
+    if (!fixedNav) {
+      position -= navHeight;
+    }
+
+    if (navHeight > 82) {
+      position += containerHeight;
+    }
+
+    window.scrollTo({
+      left: 0,
+      top: position
+    });
+    linksContainer.style.height = 0;
+  });
+});
